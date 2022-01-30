7e7b89e584ed28ca754949da59e9697c40c16283
Andrey Belianin <belyaninwork@gmail.com>
2022-01-30 12:15:14 +0500

    Detecting is point inside a polygon

--- GameEngine.Core/LinearMath.cs
+++ GameEngine.Core/LinearMath.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 
 namespace GameEngine.Core
 {
@@ -18,6 +19,70 @@ namespace GameEngine.Core
                 return null;
 
             return new PointF(p1.X + a2 * t1, p1.Y + a1 * t1);
+        }
+
+        // Не работает :/
+        public static bool IsPointInsidePolygon2(PointF point, PointF[] polygon)
+        {
+            var j = polygon.Length - 1;
+            var result = false;
+            for (int i = 0; i < polygon.Length; i++)
+            {
+                if ((polygon[i].Y <= point.Y && point.Y < polygon[j].Y || polygon[j].Y <= point.Y && point.Y < polygon[i].Y) &&
+                    point.X > (polygon[j].X - polygon[i].X) * (point.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) + polygon[i].X)
+                    result = !result;
+                j = i;
+            }
+
+            return result;
+        }
+        
+        // Пока, боюсь представить как это работает
+        public static bool IsPointInsidePolygon(PointF point, PointF[] p)
+        {
+            var i1 = 0;
+            var i2 = 0;
+            var S = 0f;
+            var S1 = 0f;
+            var S2 = 0f;
+            var S3 = 0f;
+            var flag = false;
+            for (var n = 0; n < p.Length; n++)
+            {
+                i1 = n < p.Length-1 ? n + 1 : 0;
+                while (!flag)
+                {
+                    i2 = i1 + 1;
+                    if (i2 >= p.Length)
+                        i2 = 0;
+                    if (i2 == (n < p.Length-1 ? n + 1 : 0))
+                        break;
+                    S = Math.Abs(p[i1].X * (p[i2].Y - p[n ].Y) +
+                             p[i2].X * (p[n ].Y - p[i1].Y) +
+                             p[n].X  * (p[i1].Y - p[i2].Y));
+                    S1 = Math.Abs(p[i1].X * (p[i2].Y - point.Y) +
+                              p[i2].X * (point.Y       - p[i1].Y) +
+                              point.X       * (p[i1].Y - p[i2].Y));
+                    S2 = Math.Abs(p[n ].X * (p[i2].Y - point.Y) +
+                                  p[i2].X * (point.Y       - p[n ].Y) +
+                                  point.X       * (p[n ].Y - p[i2].Y));
+                    S3 = Math.Abs(p[i1].X * (p[n ].Y - point.Y) +
+                                  p[n ].X * (point.Y       - p[i1].Y) +
+                                  point.X       * (p[i1].Y - p[n ].Y));
+                    if (Math.Abs(S - (S1 + S2 + S3)) < 0.1) 
+                    {
+                        flag = true;
+                        break;
+                    }
+                    i1 = i1 + 1;
+                    if (i1 >= p.Length)
+                        i1 = 0;
+                    break;
+                }
+                if (flag)
+                    break;
+            }
+            return flag;
         }
     }
 }
--- GameEngine.Tests/LinearMathTests.cs
+++ GameEngine.Tests/LinearMathTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using FluentAssertions;
 using GameEngine.Core;
 using GameEngine.Core.Entities;
@@ -25,6 +26,23 @@ namespace GameEngine.Tests
            var result = LinearMath.FindIntersection(e1.Position, e1.Position.Add(e1.Vector), e2.Position, e2.Position.Add(e2.Vector));
 
            result.Should().BeEquivalentTo(new PointF(50, 50));
+        }
+
+        [TestCaseSource(nameof(GetPointsInPolygon))]
+        public void DetectPoint_InPolygon(PointF point, PointF[] polygon)
+        {
+            LinearMath.IsPointInsidePolygon(point, polygon).Should().BeTrue();
+        }
+
+        public static IEnumerable<TestCaseData> GetPointsInPolygon()
+        {
+            yield return new TestCaseData(new PointF(50, 50), new[]
+            {
+                new PointF(0, 0),
+                new PointF(100, 0),
+                new PointF(0, 100),
+                new PointF(100, 100)
+            });
         }
     }
 }
